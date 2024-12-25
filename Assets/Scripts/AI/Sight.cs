159c93b30002bd54cdcf1592e2af6070de2ef62a
Nahuel De La Cruz <nahuel.delacruz@outlook.com>
2024-12-25 01:56:50 -0300

    Implementación de rango de visión del enemigo

--- Assets/Scripts/AI/Sight.cs
+++ Assets/Scripts/AI/Sight.cs
@@ -10,19 +10,29 @@ public class Sight : MonoBehaviour
     public LayerMask targetLayers;
     public LayerMask obstacleLayers;
 
+    public Collider detectedTarget;
+
     void Update()
     {
         Collider[] colliders = Physics.OverlapSphere(transform.position, distance,targetLayers);
+
+        detectedTarget = null;
 
         foreach (Collider collider in colliders)
         {
             Vector3 directionToCollider = Vector3.Normalize(collider.bounds.center - transform.position);
 
             float angleToCollider = Vector3.Angle(transform.forward, directionToCollider);
-            
-            
-            
+
+            if (angleToCollider < angle)
+            {
+                if (!Physics.Linecast(transform.position, collider.bounds.center, obstacleLayers))
+                {
+                    // Se guarda la referencia del objetivo detectado
+                    detectedTarget = collider;
+                    break;
+                }
+            }
         }
     }
-    
 }
